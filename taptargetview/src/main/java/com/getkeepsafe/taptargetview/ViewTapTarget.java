7cbc6c2d033ba8f4b53cf3cc527dee9df02de7f7
Hilal Alsibai <xiphirx@gmail.com>
2017-01-26 22:17:20 -0800

    Allow custom icons to be set on a view tap target

--- taptargetview/src/main/java/com/getkeepsafe/taptargetview/ViewTapTarget.java
+++ taptargetview/src/main/java/com/getkeepsafe/taptargetview/ViewTapTarget.java
@@ -44,7 +44,7 @@ public class ViewTapTarget extends TapTarget {
                 bounds = new Rect(location[0], location[1],
                         location[0] + view.getWidth(), location[1] + view.getHeight());
 
-                if (view.getWidth() > 0 && view.getHeight() > 0) {
+                if (icon == null && view.getWidth() > 0 && view.getHeight() > 0) {
                     final Bitmap viewBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
                     final Canvas canvas = new Canvas(viewBitmap);
                     view.draw(canvas);
