c9d9d4b3c665fdcdaed991e50d9f2b3daf08bbdc
j1gs4w <matthias_ringwald@hotmail.com>
2017-05-17 15:41:55 +0200

    indent 2

--- taptargetview/src/main/java/com/getkeepsafe/taptargetview/ViewTapTarget.java
+++ taptargetview/src/main/java/com/getkeepsafe/taptargetview/ViewTapTarget.java
@@ -23,44 +23,44 @@ import android.support.annotation.Nullable;
 import android.view.View;
 
 class ViewTapTarget extends TapTarget {
-    final View view;
+  final View view;
 
-    ViewTapTarget(View view, CharSequence title, @Nullable CharSequence description) {
-        super(title, description);
-        if (view == null) {
-            throw new IllegalArgumentException("Given null view to target");
+  ViewTapTarget(View view, CharSequence title, @Nullable CharSequence description) {
+    super(title, description);
+    if (view == null) {
+      throw new IllegalArgumentException("Given null view to target");
+    }
+    this.view = view;
+  }
+
+  @Override
+  public void onReady(final Runnable runnable) {
+    ViewUtil.onLaidOut(view, new Runnable() {
+      @Override
+      public void run() {
+        // Cache bounds
+        final int[] location = new int[2];
+        view.getLocationOnScreen(location);
+        bounds = new Rect(location[0], location[1],
+                                                                                            location[0] + view.getWidth(), location[1] + view.getHeight());
+
+        if (shape == SHAPE.RECTANGLE) {
+          targetRectWidth = view.getWidth();
+          targetRectHeight = view.getHeight();
+        } else {
+          targetRadius = (int) (Math.sqrt(view.getWidth() * view.getWidth() + view.getHeight() * view.getHeight())) / 2;
         }
-        this.view = view;
-    }
 
-    @Override
-    public void onReady(final Runnable runnable) {
-        ViewUtil.onLaidOut(view, new Runnable() {
-            @Override
-            public void run() {
-                // Cache bounds
-                final int[] location = new int[2];
-                view.getLocationOnScreen(location);
-                bounds = new Rect(location[0], location[1],
-                        location[0] + view.getWidth(), location[1] + view.getHeight());
+        if (icon == null && view.getWidth() > 0 && view.getHeight() > 0) {
+          final Bitmap viewBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+          final Canvas canvas = new Canvas(viewBitmap);
+          view.draw(canvas);
+          icon = new BitmapDrawable(view.getContext().getResources(), viewBitmap);
+          icon.setBounds(0, 0, icon.getIntrinsicWidth(), icon.getIntrinsicHeight());
+        }
 
-                if (shape == SHAPE.RECTANGLE) {
-                    targetRectWidth = view.getWidth();
-                    targetRectHeight = view.getHeight();
-                } else {
-                    targetRadius = (int) (Math.sqrt(view.getWidth() * view.getWidth() + view.getHeight() * view.getHeight()))/2;
-                }
-
-                if (icon == null && view.getWidth() > 0 && view.getHeight() > 0) {
-                    final Bitmap viewBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
-                    final Canvas canvas = new Canvas(viewBitmap);
-                    view.draw(canvas);
-                    icon = new BitmapDrawable(view.getContext().getResources(), viewBitmap);
-                    icon.setBounds(0, 0, icon.getIntrinsicWidth(), icon.getIntrinsicHeight());
-                }
-
-                runnable.run();
-            }
-        });
-    }
+        runnable.run();
+      }
+    });
+  }
 }
