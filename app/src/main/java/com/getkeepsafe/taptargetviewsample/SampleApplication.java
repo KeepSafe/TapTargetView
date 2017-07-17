9ceb8f279fc4ee43288a4fbe1ca5871cbca8503b
Hilal Alsibai <xiphirx@users.noreply.github.com>
2017-07-16 18:41:36 -0700

    Add Stetho to sample app for easier debugging

---
+++ app/src/main/java/com/getkeepsafe/taptargetviewsample/SampleApplication.java
@@ -0,0 +1,13 @@
+package com.getkeepsafe.taptargetviewsample;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+public class SampleApplication extends Application {
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    Stetho.initializeWithDefaults(this);
+  }
+}
