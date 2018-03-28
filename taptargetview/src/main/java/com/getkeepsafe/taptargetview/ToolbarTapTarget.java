07e126ec0b3dfc813e52d5216453979eaa107695
cong.nguyen <cong.nguyen@enouvo.com>
2018-03-28 12:07:21 +0700

    + add got it text for skip sequence

--- taptargetview/src/main/java/com/getkeepsafe/taptargetview/ToolbarTapTarget.java
+++ taptargetview/src/main/java/com/getkeepsafe/taptargetview/ToolbarTapTarget.java
@@ -32,23 +32,23 @@ import java.util.Stack;
 
 class ToolbarTapTarget extends ViewTapTarget {
   ToolbarTapTarget(Toolbar toolbar, @IdRes int menuItemId,
-                   CharSequence title, @Nullable CharSequence description) {
-    super(toolbar.findViewById(menuItemId), title, description);
+                   CharSequence title, @Nullable CharSequence description, @Nullable CharSequence skipText) {
+    super(toolbar.findViewById(menuItemId), title, description, skipText);
   }
 
   ToolbarTapTarget(android.widget.Toolbar toolbar, @IdRes int menuItemId,
-                   CharSequence title, @Nullable CharSequence description) {
-    super(toolbar.findViewById(menuItemId), title, description);
+                   CharSequence title, @Nullable CharSequence description, @Nullable CharSequence skipText) {
+    super(toolbar.findViewById(menuItemId), title, description, skipText);
   }
 
   ToolbarTapTarget(Toolbar toolbar, boolean findNavView,
-                   CharSequence title, @Nullable CharSequence description) {
-    super(findNavView ? findNavView(toolbar) : findOverflowView(toolbar), title, description);
+                   CharSequence title, @Nullable CharSequence description, @Nullable CharSequence skipText) {
+    super(findNavView ? findNavView(toolbar) : findOverflowView(toolbar), title, description, skipText);
   }
 
   ToolbarTapTarget(android.widget.Toolbar toolbar, boolean findNavView,
-                   CharSequence title, @Nullable CharSequence description) {
-    super(findNavView ? findNavView(toolbar) : findOverflowView(toolbar), title, description);
+                   CharSequence title, @Nullable CharSequence description, @Nullable CharSequence skipText) {
+    super(findNavView ? findNavView(toolbar) : findOverflowView(toolbar), title, description, skipText);
   }
 
   private static ToolbarProxy proxyOf(Object instance) {
