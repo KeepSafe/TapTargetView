6255c59918c9132fb8a912f8c24d741e05101de8
Joachim Klahr <joachim.klahr@gmail.com>
2019-06-05 15:51:27 +0200

    Removed black box.

--- taptargetview/src/main/java/com/getkeepsafe/taptargetview/ViewTapTarget.java
+++ taptargetview/src/main/java/com/getkeepsafe/taptargetview/ViewTapTarget.java
@@ -44,13 +44,13 @@ class ViewTapTarget extends TapTarget {
         bounds = new Rect(location[0], location[1],
             location[0] + view.getWidth(), location[1] + view.getHeight());
 
-        if (icon == null && view.getWidth() > 0 && view.getHeight() > 0) {
+        /*if (icon == null && view.getWidth() > 0 && view.getHeight() > 0) {
           final Bitmap viewBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
           final Canvas canvas = new Canvas(viewBitmap);
           view.draw(canvas);
           icon = new BitmapDrawable(view.getContext().getResources(), viewBitmap);
           icon.setBounds(0, 0, icon.getIntrinsicWidth(), icon.getIntrinsicHeight());
-        }
+        }*/
 
         runnable.run();
       }
