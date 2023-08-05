861101d2e6352b1e4e544a2f1cf5b1da45f05321
william <william@dynadot.com>
2023-08-05 13:02:59 +0800

    update gradle

--- app/src/main/java/com/getkeepsafe/taptargetviewsample/SampleApplication.java
+++ app/src/main/java/com/getkeepsafe/taptargetviewsample/SampleApplication.java
@@ -2,12 +2,12 @@ package com.getkeepsafe.taptargetviewsample;
 
 import android.app.Application;
 
-import com.facebook.stetho.Stetho;
+//import com.facebook.stetho.Stetho;
 
 public class SampleApplication extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
-    Stetho.initializeWithDefaults(this);
+//    Stetho.initializeWithDefaults(this);
   }
 }
